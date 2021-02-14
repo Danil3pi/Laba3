17f32fca64426dbdd11e2a4852ded583c2c6aa67
lo0rd <lo0rd1@bk.ru>
2021-02-14 12:26:17 +0300

    Добавлены глаза.

--- 1_draw.py
+++ 1_draw.py
@@ -1,9 +1,9 @@
 import pygame
 from pygame.draw import *
 
-SCREEN_WIDTH, SCREEN_HEIGHT = 1000, 1000
+SCREEN_WIDTH, SCREEN_HEIGHT = 500, 500
 MAIN_SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+MAIN_SCREEN.fill([255, 255, 255])
 
 def main():
     loop()
@@ -26,10 +26,15 @@ def loop():
 
 
 def draw_smile():
-    draw_face((SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2), 300)
+    """
+    Нужна система координат.
+    :return:
+    """
+    draw_face((SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2), 250)
     draw_mount()
     draw_eyes()
     draw_eyebrows()
+
 
 def draw_face(face_position : tuple, face_radius : int) -> None:
     """
@@ -39,18 +44,29 @@ def draw_face(face_position : tuple, face_radius : int) -> None:
     :return: Ничего
     """
     circle(MAIN_SCREEN, (255, 255, 1), face_position, face_radius)
+    circle(MAIN_SCREEN, 'black', face_position, face_radius, 3)
 
 
 def draw_mount():
-    pass
+    """
+    Рисует рот.
+    :return:
+    """
+    rect(MAIN_SCREEN, (0, 0, 0), (100, 300, 300, 70))
 
 
 def draw_eyes():
-    pass
+    # left eye
+    circle(MAIN_SCREEN, 'red', (150, 150), 45)
+    circle(MAIN_SCREEN, 'black', (150, 150), 15)
+
+    # right eye
+    circle(MAIN_SCREEN, 'red', (SCREEN_WIDTH - 175, 150), 30)
+    circle(MAIN_SCREEN, 'black', (SCREEN_WIDTH - 175, 150), 10)
 
 
 def draw_eyebrows():
-    pass
+    polygon(MAIN_SCREEN, 'black', [(12, 14), (140, 14), (150, 52)])
 
 main()
 pygame.quit()
